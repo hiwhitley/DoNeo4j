9066dbc88c1b9797b9b2548a40689722169ed053
hiwhitley <hiwhitley@163.com>
2016-11-26 16:56:33 +0800

    update

--- src/main/java/com/hiwhitley/graph/HangZhou.java
+++ src/main/java/com/hiwhitley/graph/HangZhou.java
@@ -7,7 +7,10 @@ import com.hiwhitley.graph.util.FileUtils;
 import org.neo4j.driver.v1.*;
 
 import java.io.File;
+import java.nio.file.AccessMode;
 import java.util.List;
+
+import static org.neo4j.driver.v1.Values.parameters;
 
 /**
  * Created by hiwhitley on 16-11-25.
@@ -16,18 +19,19 @@ public class HangZhou {
 
     public static void main(String[] args) {
 
-        Driver driver = GraphDatabase.driver( "bolt://localhost", AuthTokens.basic("neo4j", "123"), Config.build()
-                .withEncryptionLevel( Config.EncryptionLevel.REQUIRED )
-                .withTrustStrategy( Config.TrustStrategy.trustCustomCertificateSignedBy(new File( "/home/hiwhitley/ NEO4J_HOME/certificates/neo4j.cert") ) )
-                .toConfig() );
-        HangZhou hangZhou = new HangZhou();
+        Driver driver = GraphDatabase.driver("bolt://localhost", AuthTokens.basic("neo4j", "123"), Config.build()
+                .withEncryptionLevel(Config.EncryptionLevel.REQUIRED)
+                .withTrustStrategy(Config.TrustStrategy.trustCustomCertificateSignedBy(new File("/home/hiwhitley/ NEO4J_HOME/certificates/neo4j.cert")))
+                .toConfig());
 
         Session session = driver.session();
-        try(Transaction transaction = session.beginTransaction()) {
-//            Node shop =
-//            transaction.success();
+        StatementResult result;
+        try (Transaction tx = session.beginTransaction()) {
+            result = tx.run("MERGE (food:杭帮菜 {name: {name}, consumptionPerPerson: {consumption}})",
+                    parameters("name", "知味观", "consumption", 64));
+            tx.success();
+            tx.close();
         }
-
         session.close();
         driver.close();
     }
---
+++ src/main/java/com/hiwhitley/graph/Operator.java
@@ -0,0 +1,12 @@
+package com.hiwhitley.graph;
+
+/**
+ * Created by hiwhitley on 16-11-26.
+ */
+public class Operator {
+    private static StringBuilder cypher = new StringBuilder();
+
+    private addLabel() {
+
+    }
+}
